d90414c817ff6460abca024a5d928e720fbb6716
maxxiaobao <maxxiaobao@users.noreply.github.com>
2016-04-18 21:05:53 +0800

    Update Util.js

--- step2/task24/Util.js
+++ step2/task24/Util.js
@@ -1,7 +1,7 @@
 function addHandler(element,type,Handler){
 	if(element.addEventListener){
 		element.addEventListener(type,Handler,false);
-	}if(element.attachEvent){
+	}else if(element.attachEvent){
 		element.attachEvent('on'+type,Handler);
 	}else{
 		element['on'+type] = Handler;
