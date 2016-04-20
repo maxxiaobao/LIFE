bfd36b7ed4005921518a57a79021b65ed8415c5a
maxxiaobao <782860994@qq.com>
2016-04-20 16:41:45 +0800

    del

--- step2/task24/Util.js
+++
@@ -1,9 +0,0 @@
-function addHandler(element,type,Handler){
-	if(element.addEventListener){
-		element.addEventListener(type,Handler,false);
-	}else if(element.attachEvent){
-		element.attachEvent('on'+type,Handler);
-	}else{
-		element['on'+type] = Handler;
-	}
-}
